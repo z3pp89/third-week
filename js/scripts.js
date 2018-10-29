aa769b7560ed7e78578c64f6d4b9e65a7816f56a
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-28 21:01:05 -0700

    finished the 3rd week project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
+var array = [];
+var daveArray = [];
 
+
+var genArray = function(num) {
+  var count = 0;
+  for (var i = 0; i < num; i++) {
+    count++;
+    array.push(count);
+  };
+};
+
+
+var genDave = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    var iStr = (array[i]).toString();
+    if (array[i] % 3 === 0) {
+      daveArray.push("I'm sorry, Dave. I can't do that.");
+    } else if (iStr.includes(1) === true) {
+      daveArray.push("Boop!");
+    } else if (iStr.includes(0) === true) {
+      daveArray.push("Beep!")
+    } else {
+      daveArray.push(array[i]);
+    };
+  };
+};
+
+
+$(document).ready(function(){
+  $("form#userInput").submit(function(event) {
+    event.preventDefault();
+    array = [];
+    daveArray = [];
+    var num = $("#userNum").val();
+    genArray(num);
+    genDave(array);
+    $("#display").children().remove();
+    $("#display").append("<h4>" + daveArray + "</h4>");
+    this.reset();
+  });
+}); 
