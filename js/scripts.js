9c33ed70af825a37f9b6d29818ff4b262a209513
Edin Filipovic$ git config --global user.name Edin <edin.filipovic89@gmail.com>
2018-11-04 19:38:11 -0800

    Updated my code, added 0 into final result

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,49 @@
-var array = [];
-var daveArray = [];
 
 
-var genArray = function(num) {
-  var count = 0;
-  for (var i = 0; i < num; i++) {
-    count++;
-    array.push(count);
-  };
-};
+// var genArray = function(num) {
+//
+//   for (var i = 0; i < num; i++) {
+//     count++;
+//     array.push(count);
+//     console.log(count);
+//   };
+// };
 
 
-var genDave = function(array) {
-  for (var i = 0; i < array.length; i++) {
-    var iStr = (array[i]).toString();
-    if (array[i] % 3 === 0) {
-      daveArray.push("I'm sorry, Dave. I can't do that.");
-    } else if (iStr.includes(1) === true) {
-      daveArray.push("Boop!");
-    } else if (iStr.includes(0) === true) {
-      daveArray.push("Beep!")
+
+// var count = 0;
+
+function daveArray(num) {
+  var iStr = [];
+  for (var i = 0; i <= num; i++) {
+    if (i % 3 === 0 && i !=0) {
+      iStr.push("I'm sorry, Dave. I can't do that.");
+    } else if (i.toString().match(/1/)) {
+      iStr.push("Boop!");
+    } else if (i.toString().match(/0/)) {
+      iStr.push("Beep!")
+
     } else {
-      daveArray.push(array[i]);
+      iStr.push(i);
+// console.log(num);
+
     };
   };
+  return iStr;
 };
 
 
 $(document).ready(function(){
   $("form#userInput").submit(function(event) {
     event.preventDefault();
-    array = [];
-    daveArray = [];
-    var num = $("#userNum").val();
-    genArray(num);
-    genDave(array);
-    $("#display").children().remove();
-    $("#display").append("<h4>" + daveArray + "</h4>");
-    this.reset();
+    // array = [];
+    // daveArray = [];
+    var num = parseInt($("#userNum").val());
+    // genArray(num);
+    // genDave(array);
+    // $("#display").children().remove();
+    // $("#display").append("<h4>" + daveArray + "</h4>");
+    // this.reset();
+    $("#display").text(daveArray(num));
   });
-}); 
+});
